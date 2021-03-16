e8f0fd04de5415b825870c6b54c4afb60f16497d
Ronaldo jose <ronaldo.jrk@gmail.com>
2021-03-16 10:22:25 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>hello world!</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+
